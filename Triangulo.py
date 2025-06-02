2649ff17aedb4f371f180cb7829a848bd08ec97a
Cuatecontzi-Alexa <itzel.c.romero@gmail.com>
2025-06-02 10:07:07 -0600

    Add files via upload

---
+++ Triangulo.py
@@ -0,0 +1,17 @@
+class Triangulo:
+    def __init__(self):
+        self.base = 0
+        self.altura = 0
+    def leer_datos(self):
+        self.base = float(input("Ingresa la base del triángulo: "))
+        self.altura = float(input("Ingresa la altura del triángulo: "))
+
+    def calcular_area(self):
+        return (self.base * self.altura) / 2
+
+t = Triangulo()         
+t.leer_datos()         
+print("el area es:", t.calcular_area())
+
+
+
